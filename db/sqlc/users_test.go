4c146bc7aa9b4de25b02fae10d62cc9fc7dc8649
Erick Nevarez <ericknevarez97@gmail.com>
2022-09-30 13:37:59 -0500

    cleaned up tests for users

--- db/sqlc/users_test.go
+++ db/sqlc/users_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T) {
+func createTestingUser(t *testing.T) User{
 	arg := CreateUserParams{
 		UserName: "testy",
 		FullName: "Test Mcgee",
@@ -25,45 +25,66 @@ func TestCreateUser(t *testing.T) {
 	require.Equal(t, arg.FullName, user.FullName)
 	require.Equal(t, arg.Email, user.Email)
 	require.Equal(t, arg.Password, user.Password)
+
+	return user
 }
 
-func TestGetUser(t *testing.T) {
-	user, err := testQueries.GetUser(context.Background(), 1)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, user)
-
-	require.Equal(t, "testy", user.UserName)
-	require.Equal(t, "Test Mcgee", user.FullName)
-	require.Equal(t, "test@test.com", user.Email)
-	require.Equal(t, "testSecret", user.Password)
-}
-
-func TestUpdateUser(t *testing.T) {
-	arg := UpdateUserParams {
-		FullName: "TestUpdate",
-		Password: "updatePassword",
-		ID: 1,
-	}
-
-	err := testQueries.UpdateUser(context.Background(), arg)
-	require.NoError(t, err)
-
-	// Grab so we can see if it updated
-	user, getErr := testQueries.GetUser(context.Background(),1)
-	require.NoError(t, getErr)
-	require.NotEmpty(t, user)
-
-	require.Equal(t, "TestUpdate", user.FullName)
-	require.Equal(t, "updatePassword", user.Password)
-}
-
-func TestDeleteUser(t *testing.T) {
-	err := testQueries.DeleteUser(context.Background(), 1)
+func deleteTestingUser(t *testing.T, u User) {
+	err := testQueries.DeleteUser(context.Background(), u.ID)
 	require.NoError(t, err)
 
 	user, err := testQueries.GetUser(context.Background(),1)
 	require.Error(t, err)
 	require.EqualError(t, err, sql.ErrNoRows.Error())
 	require.Empty(t, user)
+
+}
+
+func TestCreateUser(t *testing.T) {
+	user := createTestingUser(t)
+	deleteTestingUser(t, user)
+}
+
+func TestGetUser(t *testing.T) {
+	rand := createTestingUser(t)
+	user, err := testQueries.GetUser(context.Background(), rand.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, rand.UserName, user.UserName)
+	require.Equal(t, rand.FullName, user.FullName)
+	require.Equal(t, rand.Email, user.Email)
+	require.Equal(t, rand.Password, user.Password)
+
+	deleteTestingUser(t, rand)
+}
+
+func TestUpdateUser(t *testing.T) {
+
+	rand := createTestingUser(t)
+
+	arg := UpdateUserParams {
+		FullName: "TestUpdate",
+		Password: "updatePassword",
+		ID: rand.ID,
+	}
+
+	err := testQueries.UpdateUser(context.Background(), arg)
+	require.NoError(t, err)
+
+	// Grab so we can see if it updated
+	user, getErr := testQueries.GetUser(context.Background(),rand.ID)
+	require.NoError(t, getErr)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Password, user.Password)
+
+	deleteTestingUser(t, rand)
+}
+
+func TestDeleteUser(t *testing.T) {
+	user := createTestingUser(t)
+	deleteTestingUser(t,user)
 }
