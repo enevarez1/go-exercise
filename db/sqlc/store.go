f53785ac99406d6c4ff5dbd4f34feef92bc0a907
Erick Nevarez <ericknevarez97@gmail.com>
2022-10-11 10:56:30 -0500

    mockstore

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,14 +2,17 @@ package db
 
 import "database/sql"
 
+type Store interface {
+	Querier
+}
 // Provides all functions to use db queries
-type Store struct {
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db: db,
 		Queries: New(db),
 	}
